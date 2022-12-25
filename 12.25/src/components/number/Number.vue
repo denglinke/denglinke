0f7d50ede6310446d5376463db2e078b4eccbd52
木木木可 <2330152138@qq.com>
2022-12-25 21:53:37 +0800

    12.25

---
+++ 12.25/src/components/number/Number.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="">
+        订单走势图
+    </div>
+</template>
+
+<script>
+export default {
+    name: "",
+    data () {
+        return {}
+    },
+    methods: {},
+    created () { },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ 12.25/src/components/region/Region.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="">
+        地区商家销售排行
+    </div>
+</template>
+
+<script>
+export default {
+    name: "",
+    data () {
+        return {}
+    },
+    methods: {},
+    created () { },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

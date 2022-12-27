48890100a51a6b72ecb05d9db2c9714ec4637cfe
木木木可 <2330152138@qq.com>
2022-12-27 23:39:13 +0800

    12.27

---
+++ 12.27/src/components/Market.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="">
+        区域销售大盘环
+    </div>
+</template>
+
+<script>
+export default {
+    name: "",
+    data () {
+        return {}
+    },
+    methods: {},
+    created () { },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

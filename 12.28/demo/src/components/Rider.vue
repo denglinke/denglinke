af03253972e8fe3063e0716c6ef5b2efcb563339
木木木可 <2330152138@qq.com>
2022-12-28 23:15:39 +0800

    12.28

---
+++ 12.28/demo/src/components/Rider.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="">
+        外卖骑手情况
+    </div>
+</template>
+
+<script>
+export default {
+    name: "",
+    data () {
+        return {}
+    },
+    methods: {},
+    created () { },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ 12.28/demo/src/views/HomeView.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="home">
+    <div class="headers">
+      <img src="../../img/头部.png">
+    </div>
+    <div class="home-one">
+      <div class="left">
+        <Usertotal></Usertotal>
+      </div>
+      <div class="right">
+        <today></today>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Usertotal from '../components/Usertotal.vue'
+import today from '../components/Today.vue'
+export default {
+  data () {
+    return {}
+  },
+  components: {
+    Usertotal,
+    today
+  },
+  methods: {},
+  created () { },
+}
+</script>
+
+<style lang="scss" scoped>
+.home {
+  width: 100%;
+  height: 100%;
+  background-color: #191919;
+
+  .home-one {
+    display: flex;
+
+    .right {
+      margin-left: 10px;
+
+    }
+  }
+
+}
+
+.headers {
+  height: 65px;
+  width: 100%;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>

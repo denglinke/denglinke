dbd29aa0aafb8619d362775283e006391529b1f9
木木木可 <2330152138@qq.com>
2023-01-05 13:42:55 +0800

    01.03

---
+++ 01.03/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <router-view />
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+body,
+html {
+  width: 100%;
+  height: 100%;
+}
+</style>
